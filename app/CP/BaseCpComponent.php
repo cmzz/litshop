74fe17fdb74d75e16b3cf1a3d70875941b332088
wz <me19@qq.com>
2021-04-08 00:34:43 +0800

    整洁代码

--- app/CP/BaseCpComponent.php
+++ app/CP/BaseCpComponent.php
@@ -3,12 +3,18 @@
 namespace LitShop\CP;
 
 use Admin;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 use LitShop\Models\AdminUser;
 use Livewire\Component;
 use Nav;
 
 class BaseCpComponent extends Component
 {
+    /**
+     * @var string[] $listeners
+     */
     protected $listeners = ['cpUserLogout' => 'userLogout'];
     public AdminUser $user;
 
@@ -26,7 +32,10 @@ class BaseCpComponent extends Component
         \View::share('topMenu', Nav::activeTopMenu());
     }
 
-    public function userLogout()
+    /**
+     * @return Redirector|Application|RedirectResponse
+     */
+    public function userLogout(): Redirector|Application|RedirectResponse
     {
         $this->notify(__('您已安全登出'));
 
